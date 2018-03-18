eac3f3970da67fdd40887aa8b6a2cfbfdbb57e0c
gbreux <contact@guillaumebreux.com>
2018-03-18 16:13:47 +0100

    Fix title and critical and remove xhr

--- _scripts/generateCritical.js
+++ _scripts/generateCritical.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const critical = require('critical');
 const penthouse = require('penthouse');
-const replace = require('replace-in-file');
 const fs = require('fs');
 
 const URL = 'http://localhost:1313/';
@@ -14,27 +12,6 @@ const sections = {
   'home'            : {defaultFile: 'index.html', files: []},
   'list'            : {defaultFile: 'carnet-de-voyage/index.html', files: []},
 };
-
-/*getFiles('public').map((file) => {
-  file = file.replace('public/', '');
-  if(!/.html$/.test(file)) {
-    return;
-  }
-  switch (file) {
-    case (file.match(/^[0-9{4}]/) || {}).input:
-      sections['post'].files.push(file);
-      break;
-    case (file.match(/^(a-propos|partenariats)/) || {}).input:
-      sections['page'].files.push(file);
-      break;
-    case (file.match(/^(index.html|pages?)/) || {}).input:
-      sections['home'].files.push(file);
-      break;
-    default:
-      sections['list'].files.push(file);
-      break;
-  }
-});*/
 
 (function getPenthoused() {
   Object.keys(sections).map((s) => {
@@ -49,46 +26,3 @@ const sections = {
     });
   })
 })();
-/*critical.generate({
-  base: './public/',
-  src: '2018/01/dorayaki-pancake-japonais-v/index.html',
-  width: 1300,
-  height: 900,
-}).then(function (output) {
-  console.log(output);
-}).error(function (err) {
-  console.log(error);
-});*/
-
-
-/*
-  critical.generate({
-    base: 'public/',
-    src: section.defaultFile,
-    css: ['public/assets/style.css'],
-    width: 1300,
-    height: 900
-  }, function (err, output) {
-    section.files.map((file) => {
-      replace.sync({
-        files: __dirname.replace('_scripts', 'public') + '/' + file,
-        from: 'REPLACE_CRITICAL',
-        to: output,
-      });
-    });
-  });*/
-
-
-function getFiles (dir, files_){
-  files_ = files_ || [];
-  const files = fs.readdirSync(dir);
-  for (const i in files){
-    const name = dir + '/' + files[i];
-    if (fs.statSync(name).isDirectory()){
-      getFiles(name, files_);
-    } else {
-      files_.push(name);
-    }
-  }
-  return files_;
-}
