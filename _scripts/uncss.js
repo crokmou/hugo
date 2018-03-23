5cd588554f6eefe7528544803dcd38a3af91e193
gbreux <contact@guillaumebreux.com>
2018-03-23 09:33:41 +0100

    Fix uncss aloglia search

--- _scripts/uncss.js
+++ _scripts/uncss.js
@@ -1,19 +1,20 @@
-const fs = require('fs');
+const fs    = require('fs');
 const uncss = require('uncss');
 
 const files   = ['public/index.html'];
 const options = {
-  csspath      : '../public/assets/',
-  stylesheets  : ['list.css'],
-  ignoreSheets : [/fonts.googleapis/],
-  timeout      : 1000,
-  htmlroot     : 'public',
-  banner       : false,
-  userAgent    : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)'
+  csspath     : '../public/assets/',
+  stylesheets : ['list.css'],
+  ignoreSheets: [/fonts.googleapis/],
+  ignore      : [/global-search.*?/],
+  timeout     : 1000,
+  htmlroot    : 'public',
+  banner      : false,
+  userAgent   : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)',
 };
 
-uncss(files, options, function (error, output) {
-  if(error) {
+uncss(files, options, function(error, output) {
+  if (error) {
     throw new Error(error);
   }
   fs.writeFileSync('public/assets/list.css', output);
